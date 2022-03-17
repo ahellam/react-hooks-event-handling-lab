2e9762e7556fe9990ec551bd5bfa0e4dee25e33f
Aaron Hellam <aaronhellam@gmail.com>
2022-03-17 09:07:09 -0700

    commit tree 3a062b65b156458700fac830b14e3f6301f1d7c5

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+
+    function focusHandler(e){
+        console.log('Good!')
+    }
+    function blurHandler(e){
+        console.log('Hey! Eyes on me!')
+    }
+  return (
+    <div>EyesOnMe
+        <button onFocus={focusHandler} onBlur={blurHandler}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React from 'react'
+
+function Keypad() {
+
+    function passwordHandler(){
+        console.log('Entering password...')
+    }
+
+  return (
+    <div>Keypad
+        <input onChange={passwordHandler} type="password" />
+    </div>
+  )
+}
+
+export default Keypad
